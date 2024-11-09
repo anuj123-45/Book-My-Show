28d3326a91ebbb398e3a98f91b46fcdceb51d615
anuj123-45 <anuj1358.be20@chikara.edu.in>
2024-11-09 11:56:13 +0530

    taiwind css setup done

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        anuj: {
+          50: '#fff3da',
+          100: '#ffdfae',
+          200: '#ffca7d',
+          300: '#ffb54b',
+          400: '#ff9f1a',
+          500: '#e68600',
+          600: '#b36800',
+          700: '#814a00',
+          800: '#4f2c00',
+          900: '#1f0d00',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
