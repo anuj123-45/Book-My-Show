cf2acef6fbe010edd07e72ce0e60025a1ef49f91
anuj123-45 <anuj1358.be20@chikara.edu.in>
2024-11-13 13:20:57 +0530

    premiere done

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import EntertainmentCardSlider from '../Entertainment/EntertainmentCard'
+import Premier from '../Premier/Premier'
 
 const Home = () => {
     return (
         <>
             <div className="conatiner p-8 ">
-                <h1 className="text-2xl font-bold text-gray-800 my-5">
+                <h1 className="text-2xl font-bold text-gray-500 my-5">
                     The best of Live Events
                 </h1>
                 <EntertainmentCardSlider />
             </div>
+
+            <div className=' bg-anuj-800 py-16'>
+                    <div className="container mx-auto px-4">
+                    <div className='hidden md:flex' >
+                <img src="https://tinyurl.com/57xkh22d" alt="premier-image" className='w-full h-full' />
+                </div> 
+                        <Premier />
+                    </div>
+                </div>
 
         </>
     )
